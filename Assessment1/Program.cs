46ceee734c9990f56ac33735d26cded53a17dba4
Ben Horner <benjaminpnicholls@gmail.com>
2021-03-02 16:43:30 +0000

    Parties stored in a class. Still algorithm to implement.

--- Assessment1/Program.cs
+++ Assessment1/Program.cs
@@ -3,23 +3,41 @@ using System.Collections.Generic;
 
 namespace Assessment1
 {
+
     class Program
     {
         static void Main(string[] args)
         {
             string[] lines = System.IO.File.ReadAllLines("input.txt");
-            List<List<string>> party = new List<List<string>>();
-            for (int x = 2; x < lines.Length - 1; x++)
+            List<Party> parties = new List<Party>();
+
+            // Extract party name, votes, and list of MEPs -- create new objects for each party
+            for (int x = 2; x < lines.Length; x++)
             {
+                // Name and votes stored in 0 and 1 indexes
                 string[] sub = lines[x].Split(",");
-                for (int y = 0; y < sub.Length; y++)
+                List<string> meps = new List<string>();
+                // Get all MEPs in a list, remove trailing semicolon
+                for (int i = 2; i < sub.Length; i++)
                 {
-                //may need to to account for ; at the end 
-                    party[x - 2][y] = sub[y];
+                    if (sub[i].Substring(sub[i].Length - 1, 1) == ";")
+                    {
+                        meps.Add(sub[i].Split(";")[0]);
+                    }
+                    else
+                    {
+                        meps.Add(sub[i]);
+                    }
                 }
+                Party p = new Party(sub[0], int.Parse(sub[1]), meps);
+                parties.Add(p);
             }
 
-            Console.WriteLine(party[2][1]);
+            //TESTING
+            foreach (Party pp in parties)
+            {
+                Console.WriteLine("name: " + pp.name + " with votes: " + pp.votes + " and MEPs: " + pp.GetMEPs());
+            }
 
             int highestVotes = 0;
             int MEPcounter = 0;
---
+++ Assessment1/party.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assessment1
+{
+    class Party
+    {
+        public string name { get; private set; }
+        public int votes { get; private set; }
+        public List<string> meps { get; private set; }
+     
+        //constructor - do we need this?
+        public Party(string Name, int Votes, List<string> MEPs)
+        {
+            name = Name;
+            votes = Votes;
+            meps = MEPs; //does this work? to copy an entire array? should we make this a list?--might have to iterate through each element
+        }
+
+        public string GetMEPs()
+        {
+            string MEPs = "";
+            foreach (string mep in meps)
+            {
+                MEPs = MEPs + ", " + mep;
+            }
+            MEPs = MEPs[2..];
+            return MEPs;
+        }
+    }
+}
